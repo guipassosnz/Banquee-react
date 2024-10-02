a0864cecec2e979af88d0456a702204e24a8b1c1
Gui Passos <guih_passos@hotmail.com>
2024-10-02 23:01:58 +1300

    Initial commit

---
+++ src/components/DummyButton/DummyButton.tsx
@@ -0,0 +1,32 @@
+function DummyButton() {
+
+return <button>Hello World</button>
+
+
+}
+
+
+export default DummyButton
+
+
+/*
+type JelloType = {
+    name: string
+}
+
+function test(a: JelloType) {
+   const username: string = 'Gio'
+    return a.name
+}
+
+test({name:'Jello'})
+
+
+
+const car: string = 'palio'
+
+const age = 20
+
+const flag = false
+
+*/
